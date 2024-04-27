2bdedb9ac1dae82dcaa35d48f385ef6a6db85af8
z891220z <z891220z@gmail.com>
2024-04-27 22:02:28 +0800

    初始化 spring boot

---
+++ LINE-AIChatBot/src/main/java/com/justinwu/lineaichatbot/LineAiChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.justinwu.lineaichatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineAiChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineAiChatBotApplication.class, args);
+	}
+
+}
---
+++ LINE-AIChatBot/src/test/java/com/justinwu/lineaichatbot/LineAiChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinwu.lineaichatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineAiChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
